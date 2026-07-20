cc002c91995d6ce13fa531a0a053be47bc1c9880
g4vrk <snegrik123@gmail.com>
2026-07-20 11:40:01 +0300

    chore(import): removed unused import

--- common/src/main/java/ac/grim/grimac/manager/ActionTracker.java
+++ common/src/main/java/ac/grim/grimac/manager/ActionTracker.java
@@ -5,7 +5,6 @@ import ac.grim.grimac.checks.type.PacketCheck;
 import ac.grim.grimac.player.GrimPlayer;
 import ac.grim.grimac.utils.data.packetentity.PacketEntity;
 import com.github.retrooper.packetevents.event.PacketReceiveEvent;
-import com.github.retrooper.packetevents.protocol.entity.type.EntityType;
 import com.github.retrooper.packetevents.protocol.entity.type.EntityTypes;
 import com.github.retrooper.packetevents.protocol.packettype.PacketType;
 import com.github.retrooper.packetevents.protocol.world.BlockFace;
