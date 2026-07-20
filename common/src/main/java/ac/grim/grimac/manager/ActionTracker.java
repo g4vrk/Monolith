f51029d4ceb939f7afccac05e08724c5c3e40c21
g4vrk <snegrik123@gmail.com>
2026-07-20 10:32:03 +0300

    refactor(tracking): checks if target entity is player

--- common/src/main/java/ac/grim/grimac/manager/ActionTracker.java
+++ common/src/main/java/ac/grim/grimac/manager/ActionTracker.java
@@ -3,7 +3,10 @@ package ac.grim.grimac.manager;
 import ac.grim.grimac.checks.Check;
 import ac.grim.grimac.checks.type.PacketCheck;
 import ac.grim.grimac.player.GrimPlayer;
+import ac.grim.grimac.utils.data.packetentity.PacketEntity;
 import com.github.retrooper.packetevents.event.PacketReceiveEvent;
+import com.github.retrooper.packetevents.protocol.entity.type.EntityType;
+import com.github.retrooper.packetevents.protocol.entity.type.EntityTypes;
 import com.github.retrooper.packetevents.protocol.packettype.PacketType;
 import com.github.retrooper.packetevents.protocol.world.BlockFace;
 import com.github.retrooper.packetevents.wrapper.play.client.WrapperPlayClientInteractEntity;
@@ -26,7 +29,12 @@ public class ActionTracker extends Check implements PacketCheck {
                     new WrapperPlayClientInteractEntity(event);
 
             if (action.getAction() == WrapperPlayClientInteractEntity.InteractAction.ATTACK) {
-                player.actionState.onAttack();
+
+                final PacketEntity entity = player.compensatedEntities.getEntity(action.getEntityId());
+
+                if (entity != null && entity.getType() == EntityTypes.PLAYER) {
+                    player.actionState.onAttack();
+                }
             }
 
         } else if (isTickPacketIncludingNonMovement(event.getPacketType())) {
